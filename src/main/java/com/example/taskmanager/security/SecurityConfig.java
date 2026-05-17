758f73855a4a67d8431185a1b33fb20b20d1414f
nather2014 <akhilnather@gmail.com>
2026-05-17 18:06:02 +0530

    add security configuration with in-memory user details and basic auth

---
+++ src/main/java/com/example/taskmanager/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.taskmanager.security;
+
+import lombok.RequiredArgsConstructor;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(requests -> requests
+                    .requestMatchers("/api/v1/tasks/**").authenticated())
+            .httpBasic(Customizer.withDefaults())
+            .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = org.springframework.security.core.userdetails.User
+            .withUsername("admin")
+            .password(passwordEncoder().encode("password"))
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+
+    }
+}
