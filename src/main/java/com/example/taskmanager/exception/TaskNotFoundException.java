03ad8813bce5729077cbc91f9b853e5f7f38936d
nather2014 <akhilnather@gmail.com>
2026-05-18 10:24:28 +0530

    refactor: remove RestControllerAdvice from TaskNotFoundException and add null check in TaskService.create method

--- src/main/java/com/example/taskmanager/exception/TaskNotFoundException.java
+++ src/main/java/com/example/taskmanager/exception/TaskNotFoundException.java
@@ -1,8 +1,5 @@
 package com.example.taskmanager.exception;
 
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
 public class TaskNotFoundException extends RuntimeException {
     public TaskNotFoundException(String message) {
         super(message);
